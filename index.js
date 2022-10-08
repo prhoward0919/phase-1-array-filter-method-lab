9422c82614035533a21e61fed973018d0b0f13d1
Paige <prhoward97@gmail.com>
2022-10-08 10:54:27 -0400

    commit tree e17c390f0f3eb9a5beb47360c2df9cbff28f10dc

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(drivers, string) {
+    const result = drivers.filter(word => 
+        word.toUpperCase() === string.toUpperCase());
+    return result;
+}
+
+function fuzzyMatch(drivers, string) {
+    const result = drivers.filter(word => word.slice(0,2) === string.slice(0,2));
+    return result;
+}
+function matchName(drivers, string) {
+    const result = drivers.filter(({name}) => name === string);
+    return result;
+
+}
